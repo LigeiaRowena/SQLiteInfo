5bf00f9699797028d7ba95d2ce2ba9a726cbd5ed
Francesca Corsini <ladyonirya@gmail.com>
2015-03-05 17:31:13 +0100

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Francesca Corsini on 05/03/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
