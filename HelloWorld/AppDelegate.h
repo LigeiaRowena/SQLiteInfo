eefa808b1b2284a9764824c6fcd73a22a954b78b
Francesca Corsini <ladyonirya@gmail.com>
2015-03-05 18:48:55 +0100

    updated the tableview with the datasource

--- HelloWorld/AppDelegate.h
+++ HelloWorld/AppDelegate.h
@@ -8,7 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 #include "MasterViewController.h"
-#import "ScaryBugDoc.h"
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
--- HelloWorld/MasterViewController.m
+++ HelloWorld/MasterViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "MasterViewController.h"
+#import "ScaryBugDoc.h"
+#import "ScaryBugData.h"
 
 @interface MasterViewController ()
 
@@ -33,11 +35,42 @@
 - (void)loadView
 {
 	[super loadView];
+	
+	// setup sample data
+	ScaryBugDoc *bug1 = [[ScaryBugDoc alloc] initWithTitle:@"Potato Bug" rating:4 thumbImage:[NSImage imageNamed:@"potatoBugThumb.jpg"] fullImage:[NSImage imageNamed:@"potatoBug.jpg"]];
+	ScaryBugDoc *bug2 = [[ScaryBugDoc alloc] initWithTitle:@"House Centipede" rating:3 thumbImage:[NSImage imageNamed:@"centipedeThumb.jpg"] fullImage:[NSImage imageNamed:@"centipede.jpg"]];
+	ScaryBugDoc *bug3 = [[ScaryBugDoc alloc] initWithTitle:@"Wolf Spider" rating:5 thumbImage:[NSImage imageNamed:@"wolfSpiderThumb.jpg"] fullImage:[NSImage imageNamed:@"wolfSpider.jpg"]];
+	ScaryBugDoc *bug4 = [[ScaryBugDoc alloc] initWithTitle:@"Lady Bug" rating:1 thumbImage:[NSImage imageNamed:@"ladybugThumb.jpg"] fullImage:[NSImage imageNamed:@"ladybug.jpg"]];
+	self.bugs = @[bug1, bug2, bug3, bug4, bug4, bug4, bug4, bug4, bug4, bug4, bug4].mutableCopy;
+
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+	// Get a new ViewCell
+	NSTableCellView *cellView = [tableView makeViewWithIdentifier:tableColumn.identifier owner:self];
+ 
+	// Since this is a single-column table view, this would not be necessary.
+	// But it's a good practice to do it in order by remember it when a table is multicolumn.
+	if( [tableColumn.identifier isEqualToString:@"BugColumn"] )
+	{
+		ScaryBugDoc *bugDoc = self.bugs[row];
+		cellView.imageView.image = bugDoc.thumbImage;
+		cellView.textField.stringValue = bugDoc.data.title;
+		return cellView;
+	}
+	return cellView;
+}
+
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+	return [self.bugs count];
 }
 
 @end
