edd7dcebbac210f50872dfaae62ddbd0b8aab1d7
Francesca Corsini <ladyonirya@gmail.com>
2015-05-09 18:31:25 +0200

    Initial Commit

---
+++ DBInfo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DBInfo
+//
+//  Created by Francesca Corsini on 09/05/15.
+//  Copyright (c) 2015 Francesca Corsini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
